a3e7664e4c160f52db5f57adbdcdf5000167b75d
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-21 10:59:56 +0500

    static first review

--- src/Review.js
+++ src/Review.js
@@ -9,7 +9,13 @@ const Review = () => {
     <article className="review">
       <div className="img-container">
         <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
       </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
     </article>
   );
 };
